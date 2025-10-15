ebe246eea16a51e3f0dcfebbf7ba70c40ee47c3d
MooseML <mattg1229@gmail.com>
2025-10-15 09:30:10 -0400

    Update path to HF HL gap model

--- scripts/download_model.py
+++ scripts/download_model.py
@@ -5,7 +5,7 @@ def download_models():
     """Downloads HOMO-LUMO Gap Backbone model from Hugging Face Hub"""
     
     # download EfficientNetV2S model
-    three_d_homo_lumo_model_path = hf_hub_download(repo_id="MooseML/EfficientNet-Cancer-Detection", filename="efficientnet_cancer_model.h5")
+    three_d_homo_lumo_model_path = hf_hub_download(repo_id="MooseML/hlgap-gnn3d-transformer-pcqm4mv2-v1", filename="hlgap-gnn3d-transformer-pcqm4mv2-v1.pt")
     homo_lumo_model = load_model(three_d_homo_lumo_model_path)
     print("HOMO-LUMO Gap prediction model downloaded and loaded!")
 
